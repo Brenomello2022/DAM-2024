c971edc46b12fe3b43e0e9ebfaa4db08673fb4b0
Breno de Mello <110641736+Brenomello2022@users.noreply.github.com>
2024-04-02 20:59:32 -0300

    Atualizações

--- gerenciador_tarefas/lib/pages/filtro_page.dart
+++ gerenciador_tarefas/lib/pages/filtro_page.dart
@@ -27,6 +27,21 @@ class _FiltroPageState extends State<FiltroPage>{
   bool _usarOrdemDecrescente = false;
   bool _alterouValores = false;
 
+  @override
+  void initState(){
+    super.initState();
+    _carregarSharedPreferences();
+  }
+
+  void _carregarSharedPreferences() async {
+    prefs = await SharedPreferences.getInstance();
+    // _prefs = prefs;
+    setState(() {
+      _campoOrdenacao = prefs.getString(FiltroPage.CHAVE_CAMPO_ORDENACAO) ?? Tarefa.campo_id;
+      _usarOrdemDecrescente = prefs.getBool(FiltroPage.CHAVE_ORDENAR_DECRESCENTE) ?? false;
+      _descricaoController.text = prefs.getString(FiltroPage.CHAVE_FILTRO_DESCRICAO) ?? '';
+    });
+  }
 
   @override
   Widget build(BuildContext context){
@@ -66,27 +81,47 @@ class _FiltroPageState extends State<FiltroPage>{
           children: [
             Checkbox(
                 value: _usarOrdemDecrescente,
-                onChanged: null,
+                onChanged: _onUsarOrdemDecrescenteChange,
             ),
             Text('Usar ordem descrescente')
           ],
         ),
          Divider(),
-         const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
+           Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
           child: TextField(
             decoration: InputDecoration(labelText: 'A descriçao começa com:'),
+            controller: _descricaoController,
+            onChanged: _onFiltroDescricaoChange,
           ),
-
         ),
       ],
     );
   }
+
+  Future<bool> _onVoltarClick() async{
+    Navigator.of(context).pop(_alterouValores);
+    return true;
+  }
+
+  void _onFiltroDescricaoChange(String? valor){
+    prefs.setString(FiltroPage.CHAVE_FILTRO_DESCRICAO, valor ?? '');
+    _alterouValores = true;
+  }
+
   void _onCampoOrdenacaoChanged(String? valor){
     prefs.setString(FiltroPage.CHAVE_CAMPO_ORDENACAO, valor ?? '');
     _alterouValores = true;
     setState(() {
       _campoOrdenacao = valor ?? '';
+    });
+  }
+
+  void _onUsarOrdemDecrescenteChange(bool? valor){
+    prefs.setBool(FiltroPage.CHAVE_ORDENAR_DECRESCENTE, valor == true);
+    _alterouValores = true;
+    setState(() {
+      _usarOrdemDecrescente = valor == true;
     });
   }
 
